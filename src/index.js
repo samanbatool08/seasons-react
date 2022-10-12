d16ef15219749102276001f8facd3a6bbbf087ef
Saman Batool <samanbatool08@gmail.com>
2022-10-12 16:35:32 -0400

    update state properties:

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      latitude: null,
+      errorMessage: ''
+    }
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ latitude: position.coords.latitude })
+      }, 
+      error => {
+        this.setState({ errorMessage: error.message })
+      }
+    )
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        Latitude: {this.state.latitude}<br />
+        Error: {this.state.errorMessage} 
+      </div>
+    ); 
+  }
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
